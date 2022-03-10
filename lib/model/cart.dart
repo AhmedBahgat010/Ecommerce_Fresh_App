b8c31bb7d8616c93d1a26c881af655ebfc748088
Ahmed bahgat <ahmdalbaz681@gmail.com>
2022-03-10 19:19:53 +0200

    يا مسهل الحال يارب

--- lib/model/cart.dart
+++ lib/model/cart.dart
@@ -1,17 +1,17 @@
 import 'package:ecommerce_fresh_app/model/item.dart';
 import 'package:flutter/cupertino.dart';
-
 class Cart with ChangeNotifier {
   List<Item> _items = [];
   double _price = 0.0;
-
   void add(Item item) {
     if (_items.contains(item)) {
+      _items.add(item);
+      print('item is not found');
+      print('000000000000000000000000000000000000000000000000000000000000000');
+    }else{
       print('item is found');
+      print('000000000000000000000000000000000000000000000000000000000000000');
     }
-    print('000000000000000000000000000000000000000000000000000000000000000');
-    _items.add(item);
-
     _price += item.price;
     notifyListeners();
   }
